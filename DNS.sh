c43a7b0c5d24728578b4090c0b3fda436b4f83b5
Ashkan Ghasemi <74142399+ashkan4707@users.noreply.github.com>
2023-01-19 11:13:39 +0400

    Create DNS.sh

---
+++ DNS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/zone_identifier/dns_records \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Key: e37d986b5aeb10c07af376fd83aa90cf165c8' --header 'X-Auth-Email: hesam.imam@gmail.com' \
+  --data '{
+  "type": "A",
+  "content": "IP",
+  "name": "tNODEID",
+  "proxied": false,
+  "ttl": 3600
+}'
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/zone_identifier/dns_records \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Key: e37d986b5aeb10c07af376fd83aa90cf165c8' --header 'X-Auth-Email: hesam.imam@gmail.com' \
+  --data '{
+  "type": "A",
+  "content": "IP",
+  "name": "vNODEID",
+  "proxied": false,
+  "ttl": 3600
+}'
+curl --request POST \
+  --url https://api.cloudflare.com/client/v4/zones/zone_identifier/dns_records \
+  --header 'Content-Type: application/json' \
+  --header 'X-Auth-Key: e37d986b5aeb10c07af376fd83aa90cf165c8' --header 'X-Auth-Email: hesam.imam@gmail.com' \
+  --data '{
+  "type": "A",
+  "content": "IP",
+  "name": "sNODEID",
+  "proxied": false,
+  "ttl": 3600
+}'
